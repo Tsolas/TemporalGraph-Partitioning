4a8f386785dd8af6b61aefc91e40a0adc795e85a
Georgios Tsolas <tsolas16@gmail.com>
2023-11-07 19:17:51 +0200

    Initial commit

---
+++ src/main/java/gr/tsolas/temporalgraphpartitioning/TemporalGraphPartitioning.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package gr.tsolas.temporalgraphpartitioning;
+
+/**
+ *
+ * @author giorgos
+ */
+public class TemporalGraphPartitioning {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
